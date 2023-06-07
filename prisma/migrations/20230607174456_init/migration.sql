c1a3ae99f6cc877a8194780479e63b6e056999ca
Bharath2511 <bharathchandra630@gmail.com>
2023-06-07 23:39:14 +0530

    initial commit

---
+++ prisma/migrations/20230607174456_init/migration.sql
@@ -0,0 +1,71 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('BASIC', 'EDITOR', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "age" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'BASIC',
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "UserPreference" (
+    "id" TEXT NOT NULL,
+    "emailUpdates" BOOLEAN NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "UserPreference_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "averageRating" DOUBLE PRECISION NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "authorId" TEXT NOT NULL,
+    "favoritedById" TEXT,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "postId" TEXT,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE INDEX "User_email_idx" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_name_age_key" ON "User"("name", "age");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserPreference_userId_key" ON "UserPreference"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_name_key" ON "Category"("name");
+
+-- AddForeignKey
+ALTER TABLE "UserPreference" ADD CONSTRAINT "UserPreference_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_favoritedById_fkey" FOREIGN KEY ("favoritedById") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Category" ADD CONSTRAINT "Category_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- script.ts
+++ script.ts
@@ -1,62 +1,35 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
-
+// userPreference: {
+//   create: {
+//     emailUpdates: true,
+//   },
+// },
 async function main() {
-  // const user = await prisma.user.create({ data: { name: "Chandra" } });
-  // console.log(user);
-  // const users = await prisma.user.createMany({
-  //   data: [
-  //     {
-  //       name: "Bharath",
-  //       email: "b@gmail.com",
-  //       isAdmin: false,
-  //       preferences: {},
-  //     },
-  //     {
-  //       name: "Chandra",
-  //       email: "c@gmail.com",
-  //       isAdmin: false,
-  //       preferences: {},
-  //     },
-  //     {
-  //       name: "Srinivas",
-  //       email: "s@gmail.com",
-  //       isAdmin: false,
-  //       preferences:{},
-  //     },
-  //   ],
-  // });
-  //   const posts = await prisma.post.createMany({
-  //     data: [
-  //       {
-  //         rating: 4.5,
-  //         createdAt: new Date(),
-  //         updatedAt: new Date(),
-  //         authorId: "690c592d-05a0-488f-ba5a-7c5d0591f044",
-  //         favoritedById: "690c592d-05a0-488f-ba5a-7c5d0591f044",
-  //       },
-  //       {
-  //         rating: 4.5,
-  //         createdAt: new Date(),
-  //         updatedAt: new Date(),
-  //         authorId: "690c592d-05a0-488f-ba5a-7c5d0591f044",
-  //         favoritedById: "690c592d-05a0-488f-ba5a-7c5d0591f044",
-  //       },
-  //       {
-  //         rating: 4.5,
-  //         createdAt: new Date(),
-  //         updatedAt: new Date(),
-  //         authorId: "690c592d-05a0-488f-ba5a-7c5d0591f044",
-  //         favoritedById: "690c592d-05a0-488f-ba5a-7c5d0591f044",
-  //       },
-  //     ],
-  //   });
-  const posts = await prisma.post.findMany();
-  console.log(posts);
-  const users = await prisma.user.findMany();
-  console.log(users);
+  await prisma.user.deleteMany();
+  const user = await prisma.user.createMany({
+    data: [
+      {
+        name: "Bharath",
+        email: "bharath@test.com",
+        age: 27,
+      },
+      {
+        name: "Chandra",
+        email: "chandra@test.com",
+        age: 27,
+      },
+    ],
+    // include: {
+    //   userPreference: true,
+    // },
+    // select: {
+    //   name: true,
+    //   userPreference: { select: { id: true } },
+    // },
+  });
+  console.log(user);
 }
-
 main()
   .catch((e) => {
     console.log(e.message);
